691cf5f0bcf4b701eaa8b9382c6e2be06db3b08b
TollyH <ptolemyhill@sky.com>
2023-07-29 01:42:23 +0100

    Update first place only for 2023

--- first_place_only.py
+++ first_place_only.py
@@ -6,15 +6,17 @@ from numpy.typing import NDArray
 from PIL import Image
 from tqdm import tqdm
 
-CANVAS_SIZE = (2000, 2000)
+CANVAS_SIZE = (3000, 2000)
+X_OFFSET = 1500
+Y_OFFSET = 1000
 PLACE_SECONDS_PER_VIDEO_FRAMES = 96  # 4 days = 3600 frames (60s at 60 fps)
-TOTAL_PLACEMENTS = 160353104  # Value for 2022, used for progress bar
+TOTAL_PLACEMENTS = 132224375  # Value for 2023, used for progress bar
 
 # 2D array of pixels
 canvas: NDArray[numpy.uint8] = numpy.array(
     [
-        [(255, 255, 255)] * CANVAS_SIZE[1]
-        for _ in range(CANVAS_SIZE[0])
+        [(255, 255, 255)] * CANVAS_SIZE[0]
+        for _ in range(CANVAS_SIZE[1])
     ],
     dtype=numpy.uint8
 )
@@ -28,13 +30,13 @@ already_seen_coords: set[tuple[int, int]] = set()
 if not os.path.exists("images"):
     os.mkdir("images")
 
-# CSV must be pre-sorted with header removed for this program to work
-with open("2022_place_canvas_history.sorted.csv", encoding="utf8") as file:
+# CSVs must be concatenated with headers removed for this program to work
+with open("2023_place_canvas_history.csv", encoding="utf8") as file:
     for line in tqdm(file, total=TOTAL_PLACEMENTS):
         # Parse line data
         split = line.replace('"', '').split(",")
         if len(split) == 5:
-            coordinate = (int(split[3]), int(split[4]))
+            coordinate = (int(split[2]) + X_OFFSET, int(split[3]) + Y_OFFSET)
             if coordinate in already_seen_coords:
                 continue
             already_seen_coords.add(coordinate)
@@ -51,7 +53,7 @@ with open("2022_place_canvas_history.sorted.csv", encoding="utf8") as file:
             )
         user_id = split[1]
         # Convert hex string to tuple of three 0..255 values
-        color = tuple(int(split[2][i + 1:i + 3], 16) for i in (0, 2, 4))
+        color = tuple(int(split[-1][i + 1:i + 3], 16) for i in (0, 2, 4))
 
         # Process line data
         time_since_last_frame += timestamp - last_placement_time
